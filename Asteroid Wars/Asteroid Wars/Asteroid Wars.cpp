4a1f23b44d9f1307198f33c974b323e011f193d8
Redbeard2794 <jayo2794@gmail.com>
2016-01-20 16:33:56 +0000

    started on per pixel collisions

--- Asteroid Wars/Asteroid Wars/Asteroid Wars.cpp
+++ Asteroid Wars/Asteroid Wars/Asteroid Wars.cpp
@@ -367,3 +367,40 @@ int main() {
 
 	return EXIT_SUCCESS;
 }
+
+//convert a sf::FloatRect to an sf::IntRect
+sf::IntRect FToIRect(const sf::FloatRect& f)
+{
+	return sf::IntRect((int)f.left, (int)f.top, (int)f.width, (int)f.height);
+}
+
+//pixel perfect collision detection(sprite1, sprite2, texture1, texture2)
+bool PixelPerfectCollision(const sf::Sprite& spriteA, const sf::Sprite& spriteB, const sf::Image& imgA, const sf::Image& imgB)
+{
+	//https://gist.github.com/ernestoalejo/3033095
+
+	sf::IntRect boundsA(FToIRect(spriteA.getGlobalBounds()));//convert spriteA's global bounds to an sf::IntRect
+
+	sf::IntRect boundsB(FToIRect(spriteB.getGlobalBounds()));//convert spriteB's global bounds to an sf::IntRect
+	
+	sf::IntRect intersection;//An sf::intRect representing the intersection area
+
+	if (boundsA.intersects(boundsB, intersection))//if boundsA intersects boundsB, with the intersection area stored in intersection
+	{
+		const sf::Transform& inverseA(spriteA.getInverseTransform());//get spriteA's inverse transform
+
+		const sf::Transform& inverseB(spriteB.getInverseTransform());//get spriteB's inverse transform
+
+		const sf::Vector2u& sizeA(imgA.getSize());//Get the size of imgA
+
+		const sf::Vector2u& sizeB(imgB.getSize());//get the size of imgB
+
+		const sf::Uint8* pixA = imgA.getPixelsPtr();//Get imgA's pixel pointer which is a read-only pointer to its array of pixels
+
+		const sf::Uint8* pixB = imgB.getPixelsPtr();//Get imgB's pixel pointer which is a read-only pointer to its array of pixels
+
+	}
+
+
+	return false;
+}
